71f2db7592c92b876037260f60ed7cd6460d87c9
ZohraGhafour <65776600+ZohraGhafour@users.noreply.github.com>
2021-04-13 15:06:17 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,9 @@
+import { Ghafour } from '../../app/ghafour';
+
+export const MYINFO: Ghafour = {
+  sid: "991509533",
+  sname: "Zohra Ghafour",
+  slogin: "ghafour",
+  scampus: "Davis",
+  atitle: "Assignment 4"
+}
